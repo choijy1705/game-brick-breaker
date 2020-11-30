e544309d147ea4c12ee1b5bff7ba52da3929752b
Junyoung Choi <cjy1705@naver.com>
2020-11-30 11:02:25 +0900

    feat : create MapGenerator

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -14,6 +14,15 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
     private Timer timer;
     private int delay = 8;
 
+    private int playerX = 310;
+
+    private int ballposX = 120;
+    private int ballposY = 350;
+    private int ballXdir = -1;
+    private int ballYdir = -2;
+
+    private MapGenerator map;
+
 
     public GamePlay(){
 
