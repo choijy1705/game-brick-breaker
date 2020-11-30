e035a6c4a9fdb083d25e308c8e35365225d03314
Junyoung Choi <cjy1705@naver.com>
2020-11-30 10:54:14 +0900

    Initial commit

---
+++ src/GamePlay.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class GamePlay extends JPanel implements KeyListener, ActionListener {
+
+    private boolean play = false;
+    private int score = 0;
+
+    private int totalBricks = 40;
+
+    private Timer timer;
+    private int delay = 8;
+
+
+    public GamePlay(){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
